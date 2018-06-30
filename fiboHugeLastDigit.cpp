bfc8599d24f53df1f7f3cf60ea493614f2fa7883
Ritika Mehta <mehtarit@umich.edu>
2018-06-30 14:05:57 -0500

    Figured out last digitof sum of fibonacci numbers

--- fiboHugeLastDigit.cpp
+++ fiboHugeLastDigit.cpp
@@ -2,12 +2,39 @@
 
 using namespace std;
 
+
+void pre_compute(int arr[])
+{
+	int n = 60;
+	arr[0] = 0;
+	arr[1] = 1;
+	int previous = 0;
+	int current = 1;
+	int sum_last_digit = 1;
+
+	for(int i = 2; i < n-1; i++)
+	{
+		int temp_previous = previous % 10;
+		previous = current % 10;
+		current = temp_previous + previous;
+		sum_last_digit = sum_last_digit % 10 + current % 10;
+		arr[i] = sum_last_digit %10;
+	}
+
+}
+
 int main()
 {
 	long long n;
 
+	//pisano period of 10 is 60
+	int arr[60];
+	pre_compute(arr);
+
 	cin >> n;
-	cout << n << endl;
+
+	int n_mod_sixty = n % 60;
+	cout << arr[n_mod_sixty] << endl;
 
 	return 0;
 }
